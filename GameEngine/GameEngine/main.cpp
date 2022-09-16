368f9e6c66326f099914c7aa573c4c8d2fff7edd
PJWallace1 <90213252+PJWallace1@users.noreply.github.com>
2022-09-16 18:18:12 -0500

    square moves

--- GameEngine/GameEngine/main.cpp
+++ GameEngine/GameEngine/main.cpp
@@ -1,22 +1,33 @@
 #include <SFML/Graphics.hpp>
 
+const int WINOW_W = 1000;
+const int WINDOW_H = 1000;
+const int FPS_LIMIT = 60;
+const float PLAYER_SPEED = 2;
+
+using namespace sf;
+
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "Game Engine");
-  window.setFramerateLimit(60);
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  RenderWindow window(VideoMode(WINOW_W, WINDOW_H), "Game Engine");
+  window.setFramerateLimit(FPS_LIMIT);
+  RectangleShape rectangle;
+  rectangle.setSize(sf::Vector2f(100, 50));
+  rectangle.setOutlineColor(sf::Color::Red);
+  rectangle.setOutlineThickness(5);
+  rectangle.setPosition(10, 20);
+  rectangle.setFillColor(Color::Green);
 
   while (window.isOpen())
   {
-    sf::Event event;
+    Event event;
     //while there are pending events
     while (window.pollEvent(event))
     {
       switch (event.type)
       {
         // window closed
-        case sf::Event::Closed:
+        case Event::Closed:
           window.close();
           break;
         // we don't process other types of events
@@ -24,9 +35,22 @@ int main()
           break;
       }
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+      rectangle.move(PLAYER_SPEED, 0.f);
+    } 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+      rectangle.move(PLAYER_SPEED * -1, 0.f);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+      rectangle.move(0.f, PLAYER_SPEED * -1);
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+      rectangle.move(0.f, PLAYER_SPEED);
+    }
+
 
     window.clear();
-    window.draw(shape);
+    window.draw(rectangle);
     window.display();
   }
 
